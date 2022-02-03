318e1160088084c53263f95bf0c0057bbab2d6e1
opengl-8080 <opengl-8080@users.noreply.github.com>
2022-02-03 12:00:50 +0900

    test: JUnit5対応 (#16)

--- src/test/java/com/nablarch/example/sample/mom/EmbeddedMessagingProviderTest.java
+++ src/test/java/com/nablarch/example/sample/mom/EmbeddedMessagingProviderTest.java
@@ -1,29 +1,37 @@
 package com.nablarch.example.sample.mom;
 
-import static org.junit.Assert.assertNotNull;
-
 import org.apache.activemq.broker.BrokerService;
 
 import nablarch.core.repository.SystemRepository;
 import nablarch.fw.messaging.MessagingContext;
 import nablarch.test.RepositoryInitializer;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 
 /**
  * {@link EmbeddedMessagingProviderTest}のテストクラス。
  */
-public class EmbeddedMessagingProviderTest {
+class EmbeddedMessagingProviderTest {
+    EmbeddedMessagingProvider embeddedMessagingProvider;
 
-    @Before
-    public void setUp() {
+    @BeforeEach
+    void setUp() {
         RepositoryInitializer.reInitializeRepository("EmbeddedMessagingProvider.xml");
+        embeddedMessagingProvider = SystemRepository.get("messagingProvider");
     }
 
-    @After
-    public void tearDown() throws Exception {
+    @AfterEach
+    void tearDown() throws Exception {
+        if (embeddedMessagingProvider != null) {
+            //後続のテストのためにサーバを停止する。
+            embeddedMessagingProvider.stopServer();
+        }
+
         RepositoryInitializer.revertDefaultRepository();
     }
 
@@ -31,45 +39,29 @@ public class EmbeddedMessagingProviderTest {
      * プロバイダの初期化が可能であることを確認する。
      */
     @Test
-    public void testInitialize() {
-        EmbeddedMessagingProvider embeddedMessagingProvider = null;
-        try {
-            embeddedMessagingProvider = SystemRepository.get("messagingProvider");
-            embeddedMessagingProvider.initialize();
+    void testInitialize() {
+        embeddedMessagingProvider.initialize();
 
-            //プロバイダ初期化の成功確認のために、コンテキストを取得してみる。
-            MessagingContext context = embeddedMessagingProvider.createContext();
-            assertNotNull(context);
-        } finally {
-            if (embeddedMessagingProvider != null) {
-                //後続のテストのためにサーバを停止する。
-                embeddedMessagingProvider.stopServer();
-            }
-        }
+        //プロバイダ初期化の成功確認のために、コンテキストを取得してみる。
+        MessagingContext context = embeddedMessagingProvider.createContext();
+        assertNotNull(context);
     }
 
     /**
      * 既に使用済みのポートを使用しようとした場合、実行時例外が送出されることを確認する。
-     *
-     * @throws Exception
      */
-    @Test(expected = RuntimeException.class)
-    public void testInitializeUsedPort() throws Exception {
-        EmbeddedMessagingProvider embeddedMessagingProvider = null;
+    @Test
+    void testInitializeUsedPort() throws Exception {
         BrokerService broker = new BrokerService();
         try {
             broker.setPersistent(false);
             broker.setUseJmx(false);
             broker.addConnector("tcp://localhost:61616");
-            embeddedMessagingProvider = SystemRepository.get("messagingProvider");
-            embeddedMessagingProvider.initialize();
+
+            assertThrows(RuntimeException.class, () -> embeddedMessagingProvider.initialize());
         } finally {
             broker.stop();
             broker.waitUntilStopped();
-            if (embeddedMessagingProvider != null) {
-                //後続のテストのためにサーバを停止する。
-                embeddedMessagingProvider.stopServer();
-            }
         }
     }
 }
